f01f98ecbc7e3727bca9427dce66182bee7d9ae9
jin09 <supergautam995@gmail.com>
2016-11-14 23:29:45 +0530

    fixed app crash, added new folders

--- fileorganizer.py
+++ fileorganizer.py
@@ -5,10 +5,12 @@ images = ["JPG", "JPEG", "GIF", "PNG", "SVG"]
 audio = ["MP3", "WAV", "WMA", "MKA", "AAC", "MID", "RA", "RAM", "RM", "OGG"]
 video = ["FLV", "WMV", "MOV", "MP4", "MPEG", "3GP", "MKV"]
 code = ["CPP", "RB", "PY", "HTML", "CSS", "JS"]
-sys_files = ["DEB", "EXE", "SH", "BUNDLE"]
+sys_files = ["DEB", "SH", "BUNDLE"]
+executables = ["EXE","MSI"]
 compressed = ["RAR", "JAR", "ZIP", "TAR", "MAR", "ISO", "LZ", "7ZIP", "TGZ", "GZ", "BZ2"]
 subtitles = ["SRT"]
 torrent = ["TORRENT"]
+shortcut = ["LNK"]
 
 
 def create_folders():
@@ -34,27 +36,15 @@ def create_folders():
     list_dir.append(torrent_folder)
     misc_folder = current + "\\misc"
     list_dir.append(misc_folder)
-
+    shortcut_folder = current + "\\shortcut"
+    list_dir.append(shortcut_folder)
+    executable_folder = current + "\\executable"
+    list_dir.append(executable_folder)
     for i in list_dir:
         if not os.path.exists(i):
             os.makedirs(i)
 
-"""
-Creates a dictionary of file types of the form:
 
-{'RAM': 'audio', 'MKA': 'audio', 'DOC': 'docs', 'MID': 'audio', 'FLV': 'video',
- 'MKV': 'video', 'MP4': 'video', 'EXE': 'sys_files', 'ISO': 'compressed', 'MP3': 'audio',
-  'PDF': 'docs', 'CPP': 'code', 'DEB': 'sys_files', 'XLS': 'docs', 'TORRENT': 'torrent',
-   'AAC': 'audio', 'TAR': 'compressed', 'ODP': 'docs', 'ODS': 'docs', 'ODT': 'docs', 'HTML': 'code',
-    'RAR': 'compressed', 'RA': 'audio', 'RB': 'code', 'PPT': 'docs', 'RM': 'audio', 'TXT': 'docs',
-     'LZ': 'compressed', 'OUT': 'docs', 'PPTX': 'docs', 'XLSX': 'docs', 'DOCX': 'docs', 'ZIP': 'compressed',
-      '7ZIP': 'compressed', 'SVG': 'images', 'JAR': 'compressed', 'WMV': 'video', 'JPEG': 'images',
-       'GZ': 'compressed', 'GIF': 'images', 'MAR': 'compressed', 'BUNDLE': 'sys_files', 'SRT': 'subtitles',
-        'IN': 'docs', 'WAV': 'audio', 'WMA': 'audio', 'JS': 'code', 'CSS': 'code', 'MPEG': 'video',
-         'BZ2': 'compressed', 'MOV': 'video', 'JPG': 'images', 'TGZ': 'compressed', 'SH': 'sys_files',
-          'PY': 'code', 'OGG': 'audio', '3GP': 'video', 'PAGES': 'docs', 'PNG': 'images'}
-
-"""
 def create_dictionary_of_file_types():
     hash = {}
     for i in docs:
@@ -75,6 +65,10 @@ def create_dictionary_of_file_types():
         hash[i] = "subtitles"
     for i in torrent:
         hash[i] = "torrent"
+    for i in executables:
+    	hash[i] = "executable"
+    for i in shortcut:
+    	hash[i] = "shortcut"
     print(hash)
     return hash
 
@@ -96,10 +90,17 @@ def put_files_in_folder():
                 if file_type in dictionary:
                     actual_destination_folder = dictionary[file_type]
                     if file_name != "fileorganizer":
-                        os.rename(os.getcwd() + "\\" + i, os.getcwd() + "\\" + actual_destination_folder + "\\" + i)
+                    	try:
+                        	os.rename(os.getcwd() + "\\" + i, os.getcwd() + "\\" + actual_destination_folder + "\\" + i)
+                        except:
+                        	print("unsuccessful-"+i)
                 else:
                     if file_name != "fileorganizer":
-                        os.rename(os.getcwd() + "\\" + i, os.getcwd() + "\\misc\\" + i)
+                    	try:
+                        	os.rename(os.getcwd() + "\\" + i, os.getcwd() + "\\misc\\" + i)
+                        except:
+                        	print("unsuccessful-"+i)
+                        	
 
 create_folders()
 put_files_in_folder()
